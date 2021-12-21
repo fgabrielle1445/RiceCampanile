163c1050d6992923e42bc560ab9b87e1baee87fe
Gabby <fgabrielle1445@yahoo.com>
2021-12-20 18:21:53 -0700

    Made the pages

---
+++ rice-campanile/src/components/Footer.js
@@ -0,0 +1,24 @@
+function Footer() {
+
+    return(
+        <div>
+            <div>
+                <h2>The Campanile</h2>
+            </div>
+            <div>
+                <p>6100 Main St</p>
+                <p>Houston, TX 77005</p>
+                <p>yearbook@rice.edu</p>
+                <p>(713) 348 6097</p>
+            </div>
+            <div>
+                <p>FaceBook</p>
+                <p>Spotify</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    );
+    
+}
+
+export default Footer;
---
+++ rice-campanile/src/pages/Home.js
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer";
+
+function Home() {
+
+    return(
+        <div>
+            <div>
+            <h1>It's not just an urban legend.</h1>
+            <h3>It's in the Campanile.</h3>
+            <button>About us</button>
+            </div>
+            <Footer/>
+        </div>
+    );
+
+}
+ export default Home;
